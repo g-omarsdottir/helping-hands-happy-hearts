601f54c9113628b0e189024b29fa0c0c3863347a
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-05-17 18:41:44 +0000

    Add html file, form class and view for CRUD functionality
    - Add file add_post.html for users to add posts
    - Add form class to define form fields displayed to users to add posts (incomplete)
    - Add view for adding posts and define user, preventing users to choose other usernames as author

--- board/forms.py
+++ board/forms.py
@@ -1,12 +1,38 @@
 from django.forms import ModelForm
+from django import forms
+from djrichtextfield.widgets import RichTextWidget
 from cloudinary.forms import CloudinaryFileField
 from .models import Post
 
 # Check: Code from Cloudinary tutorial, implements a form on website. Check later if needed.
+class PostForm(forms.ModelForm):
+    """
+    Form to create a post
+    """
+
+    class Meta:
+        """
+        Defines the form fields
+        """
+        model = Post
+        fields = [
+            'title', 'excerpt', 'content', 'status', 'location',
+            'category', 'subcategory', 'post_image', 'availability',
+            'tools_required', 'remarks', 'target_date',
+        ]
+        # excerpt = forms.CharField(widget=RichTextWidget())
+        # content = forms.CharField(widget=RichTextWidget())
+        widgets = {
+            'excerpt': forms.RichTextWidget(),
+            'content': forms.RichTextWidget(),
+        }
+
+
 class PhotoForm(ModelForm):
     """
     Defines the form field for the image upload field.
     Specifies the formal handle file upload and utilizes Cloudinary for storage.
+    Basic code structure from Cloudinary docs.
     Related to :model:`Post`
     """
     image = CloudinaryFileField(required=False)
--- board/views.py
+++ board/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic import CreateView
+from .models import Post
 
 # Create your views here.
 class BoardView(generic.TemplateView):
@@ -11,3 +13,23 @@ class BoardView(generic.TemplateView):
 
     """
     template_name = 'board/board.html'
+
+
+class AddPost(CreateView):
+    """
+    Add Post view.
+    ***Template:***
+    :template:`board/add_post.html`
+    """
+    template_name = 'board/add_post.html'
+    model = Post
+    success_url ='/post/'
+
+    def form_valid(self, form):
+        """
+        Defines the username posting the post
+        Prevents user being able to choose other usernames as author
+        """
+        def form_valid(self, form):
+            form.instance.user = self.request.user
+            return super(AddPost, self).form_valid(form)
