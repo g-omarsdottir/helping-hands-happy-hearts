2e3b526cc5dac452f28afc4b83bb6c6a87a908e4
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-05-19 10:58:46 +0000

    Update the placeholder generic Board view and url

--- board/urls.py
+++ board/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from .views import AddPost
+from .views import AddPost, Board
 
 
 urlpatterns = [
     path('add/', AddPost.as_view(), name='add_post'),
-    path('', views.BoardView.as_view(), name='board'),
+    path('board/', views.Board.as_view(), name='board'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
 from django.views import generic
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 from .forms import PostForm, PhotoForm # check: PhotoForm
 
 # Create your views here.
-class BoardView(generic.TemplateView):
+class Board(ListView):
     """
     Display content Offers & Requests
-    Might be ListView (check: home/views)
+    Context object name customized for readability.
     ***Template:***
     :template:`board/board.html`
 
     """
     template_name = 'board/board.html'
+    model = Post
+    context_object_name = 'board'
 
 
-class AddPost(CreateView):
+class AddPost(LoginRequiredMixin, CreateView):
     """
-    Add Post view.
+    Display Add Post view.
+    LoginRequiredMixin checks if user is logged in before running CreateView.
+    If user is not logged in and attempts to add post, it redirects user to login page.
+    CreateView handles form to add a post in the DOM.
     ***Template:***
     :template:`board/add_post.html`
     """
