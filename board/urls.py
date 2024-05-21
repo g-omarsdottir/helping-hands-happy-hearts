a45922cb6126b4d24d5640a20f5677314f592215
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-05-21 10:28:17 +0000

    Add confirm deletion of post
    - Add content: form to confirm deletion of post and bootstrap style to post_confirm_delete.html
    - Add url for confirmation of post deletion to urls.py with post slug and post primary key
    - Add url and post.id for confirmation of post deletion to post_detail.html

--- board/urls.py
+++ board/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import AddPost, Board, PostDetail
+from .views import AddPost, Board, PostDetail, DeletePost
 
 
 urlpatterns = [
     path("add/", AddPost.as_view(), name="add_post"),
     path("board/", views.Board.as_view(), name="board"),
     path("<slug:pk>/", PostDetail.as_view(), name="post_detail"),
+    path("delete/<slug:pk>/", DeletePost.as_view(), name="delete_post"),
 ]
